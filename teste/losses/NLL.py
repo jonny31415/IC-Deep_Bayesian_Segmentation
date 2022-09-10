3f9251460275ae53efd0d52966b61ed7cb66cc8e
João Lucas Foltran Consoni <65779624+jonny31415@users.noreply.github.com>
2022-09-10 17:21:56 -0300

    Delete teste directory

--- teste/losses/NLL.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-def NLL(y_true, y_pred):
-    return -y_pred.log_prob(y_true)
-
-def kl_approx(q, p, q_tensor):
-    return tf.reduce_mean(q.log_prob(q_tensor) - p.log_prob(q_tensor))
